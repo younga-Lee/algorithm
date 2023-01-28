d320c1ecbb09aaae70bf080535290b5847cb04b0
younga-Lee <66080449+younga-Lee@users.noreply.github.com>
2023-01-28 17:27:15 +0900

    [level 0] Title: 소인수분해, Time: 0.29 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = []
+    for i in range(2,n+1):
+        while n%i == 0:
+            answer.append(i)
+            n = n/i
+    return sorted(set(answer))
+            
