b8224987201d2436e314a1d9a0da1ad2b839888e
younga-Lee <66080449+younga-Lee@users.noreply.github.com>
2024-01-12 01:01:29 +0900

    [level Lv.3] Title: 네트워크, Time: 0.60 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
@@ -0,0 +1,28 @@
+def solution(n, computers):
+    ans = 0
+    stk = [0]
+    while computers != [[0]*n]*n:
+        #시작하는 위치
+        for i in range(n):
+            if computers[i] != [0]*n:
+                stk.append(i)
+                ans += 1
+                break
+        #노드 확인
+        while stk:
+            idx = stk[-1]
+            computers[idx][stk[0]] = 0
+            computers[idx][idx] = 0
+            for j in range(n):
+                if computers[idx][j] == 1:
+                    computers[idx][j] = 0
+                    stk.append(j)
+                    break
+            else:
+                while stk:
+                    top = stk.pop(0)
+                    if computers[top] != [0] * n:
+                        stk.append(top)
+                        break
+
+    return ans
